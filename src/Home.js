aee537ca48fd2ff5a2f11ddea9b2f40fc0e0717e
Russell Waring <russell.waring@dcmail.ca>
2025-08-19 21:28:05 -0400

    Video 10 - Outputting Listss

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,27 @@ import { useState } from 'react';
 
 const Home = () => {
 
-  /* Variable not REACTIVE */
-  // let name = 'Luigi';
-
-  const [name, setName] = useState('Luigi');
-
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-
-    setName('Mario');
-    setAge(30);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'Bananaly', id: 1},
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Orangely', id: 2},
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Plumly', id: 3},
+  ]);
 
 
-  /* To update message dynamically, we use a hook. Hook is called useState */
+  /* Map method fires a callback function for each item (in array) */
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+
+        </div>
+
+      ))};
+
+
     </div>
   );
 }
