40e4fa8d156e8707edcff767c7fd9700d9ba773c
Russell Waring <russell.waring@dcmail.ca>
2025-08-27 10:16:10 -0400

    Video 16 - Using JSON Server

--- src/Home.js
+++ src/Home.js
@@ -9,26 +9,19 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Bananaly', id: 3},
   ]);
 
-  /*  */
-  const [name, setName] = useState('mario');
-
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
-  /* empty dependency array causes useEffect to run only on initialization */
   useEffect(() => {
     console.log('use effect ran')
-    console.log(name)
-  }, [name]);
+  }, []);
 
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{ name }</p>
     </div>
   );
 }
