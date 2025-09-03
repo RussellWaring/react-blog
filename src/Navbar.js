4b7a91fe24324a65ac11b0c4e3dbbebfcee358f1
Russell Waring <russell.waring@dcmail.ca>
2025-09-03 12:20:08 -0400

    Video 23 - Router Links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,16 @@
+/**
+ * Rather than typical href (relies on server), leverages special, React-specific link tags
+ */
+
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Yokel Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create" style={{
-          color: "white",
-          backgroundColor: '#f1356d',
-          borderRadius: '8px'
-        }}>New Blog</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </nav>
   );
