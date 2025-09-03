188cc01be4068a89405efb7b80d8249de5a2760b
Russell Waring <russell.waring@dcmail.ca>
2025-09-03 11:50:17 -0400

    Video 20 - Making a Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,17 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
 
-  /* Returns a 'promise'*'. Can't make this async i.e., use a 'wait' */
-  useEffect(() => {
-    setTimeout(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-          console.log(res);
-          if (!res.ok) {
-            throw Error('could not fetch the data for that resource');
-          }
-          return res.json();
-        })
-        .then(data =>{
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch(err => {
-          setIsPending(false);
-          setError(err.message);
-          // console.log(err.message);
-        }) // catches network error, fires function
-    }, 1000);
-  }, []);
 
-
-  /* Conditional rendering */
   return (
     <div className="home">
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      { blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 }
---
+++ src/useFetch.js
@@ -0,0 +1,39 @@
+/** 
+ * @author Russell Waring 
+ * @description Externalized logic to support custom hook. For fetching 'blog posts'.
+ */
+
+import { useState, useEffect } from 'react';
+
+/* custom hooks NEED to start with 'use' */
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+        fetch(url)
+        .then(res => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error('could not fetch the data for that resource');
+          }
+          return res.json();
+        })
+        .then(data =>{
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch(err => {
+          setIsPending(false);
+          setError(err.message);
+        })
+    }, 1000);
+  }, [url]);
+
+  return { data, isPending, error }
+}
+
+export default useFetch;
